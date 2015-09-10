6816ad908fddafb3b3dbb5ab91fac8b64df86c28
Leo Correa <lcorr005@gmail.com>
2015-09-10 14:48:07 -0400

    Double redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  constraints subdomain: false do
+    get ':any', to: redirect(subdomain: 'www', path: '/%{any}'), any: /.*/
+  end
+
+  root to: "homes#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
